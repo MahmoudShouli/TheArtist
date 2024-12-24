f7fe696294b8fc34a7f346bf361028e7ecc2689c
Mahmoud Shouli <mahmoud.shouli.yes@gmail.com>
2024-12-24 13:57:11 +0200

    ...

--- sendData.py
+++ sendData.py
@@ -18,16 +18,24 @@ except Exception as e:
 def index():
     return render_template('simple.html')  # Load the HTML form
 
-@app.route('/send', methods=['POST'])
+@app.route('/A3', methods=['POST'])
 def send_to_arduino():
     if ser is None:
         return "Error: Arduino not connected."
-
-    # Get the text from the form
-    text = request.form['text']
     try:
-        ser.write(text.encode())  # Send text to Arduino
-        return f"Sent to Arduino: {text}"
+        ser.write('A3')  # Send text to Arduino
+        return f"Sent to Arduino: A3"
+    except Exception as e:
+        return f"Error: {e}"
+    
+
+@app.route('/A4', methods=['POST'])
+def send_to_arduino():
+    if ser is None:
+        return "Error: Arduino not connected."
+    try:
+        ser.write('A4')  # Send text to Arduino
+        return f"Sent to Arduino: A4"
     except Exception as e:
         return f"Error: {e}"
 
