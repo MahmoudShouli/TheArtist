4a9d981b0630ee99195b7eadd37b56f41c867095
MahmoudShouli <mahmoud.shouli.yes@gmail.com>
2025-01-02 15:53:44 +0200

    ..

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import time
 app = Flask(__name__)
 
 # Initialize the camera
-picam = Picamera2()
+#picam = Picamera2()
 
 # Directory for storing photos and generated files
 output_dir = "static/photos"
