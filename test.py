76c04ae267ef415fe72df4fe903a451c5ab74729
Mahmoud Shouli <mahmoud.shouli.yes@gmail.com>
2025-01-15 13:08:32 +0200

    ..

--- test.py
+++ test.py
@@ -24,20 +24,14 @@ def configure_grbl(serial_port, baud_rate=115200):
             print("Current GRBL Settings:")
             print(response)
             
-            # Set absolute positioning mode (G90)
-            ser.write(b"G90\n")
-            time.sleep(0.1)
-            print("Sent G-code: G90")
-            print(ser.readline().decode('utf-8').strip())  # Read GRBL response
             
             # Send the sequence of G-code commands
             gcode_commands = [
-                "G00 Y39",      # Move to Y39
-                "G00 X300",     # Move to X300
-                "G00 Z4.5",     # Move to Z4.5
-                "G00 X308",     # Move to X308
+                "G10 L20 P1 X0 Y0 Z0",      # Move to Y39
+                "G90",     # Move to X300
+                "G0 X100",     # Move to Z4.5
+                "G0 X200",     # Move to X308
                 "M3 S0",        # Turn spindle off
-                "G00 Z0",       # Move Z to 0
                 "G00 X0 Y0"     # Move back to the origin (0,0)
             ]
             
