878d2309c131d430c9575e12f3de32c71301f1f4
Mahmoud Shouli <mahmoud.shouli.yes@gmail.com>
2025-01-27 17:08:36 +0200

    ..

--- app.py
+++ app.py
@@ -196,7 +196,7 @@ def shoot():
 
     # Increase contrast and brightness (values can be adjusted)
     alpha = 1.5  # Contrast control (1.0 = no change, >1.0 = more contrast)
-    beta = 20    # Brightness control (0 = no change, >0 = brighter)
+    beta = 15    # Brightness control (0 = no change, >0 = brighter)
     bright_contrast_image = cv2.convertScaleAbs(sharpened_image, alpha=alpha, beta=beta)
 
     # Save the processed image
